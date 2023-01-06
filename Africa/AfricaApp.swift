d656150317f89fb6a8c185f69de4e98657836a56
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-01-06 17:42:33 +0100

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Majka on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
