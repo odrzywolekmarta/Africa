20a1205554b556e799fd50d452222f212644c417
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-01-10 01:49:55 +0100

    add video model, populate list view

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct VideoListView: View {
+    //MARK: - PROPERTIES
+    var videos: [VideoModel] = Bundle.main.decode("videos.json")
+    
+    //MARK: - BODY
     var body: some View {
-        Text("Video")
-    }
+        NavigationView {
+            List(videos) { video in
+                VideoListItemView(video: video)
+                    .padding(.vertical, 8)
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    } // navigation list
 }
 
+//MARK: - PREVIEW
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
