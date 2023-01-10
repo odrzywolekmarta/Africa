6cbeb5ec6b24427f28aff13f21ce5d18845b258a
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-01-10 18:34:26 +0100

    add map to mapview

---
+++ Africa/View/MapAnnotationView.swift
@@ -0,0 +1,20 @@
+//
+//  MapAnnotationView.swift
+//  Africa
+//
+//  Created by Majka on 10/01/2023.
+//
+
+import SwiftUI
+
+struct MapAnnotationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapAnnotationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapAnnotationView()
+    }
+}
