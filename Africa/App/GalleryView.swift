3aabeb6f20471d50b0ce4d01add7b4f3b2c5a8a1
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-01-11 19:12:10 +0100

    add gallery

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,11 +9,50 @@ import SwiftUI
 
 struct GalleryView: View {
     //MARK: - PROPERTIES
+    let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+    @State private var selectedAnimal: String = "lion"
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(Array(repeating: .init(.flexible()), count: Int(gridColumn)))
+    }
     
     //MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack(alignment: .center, spacing: 30) {
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding()
+                    .onChange(of: gridColumn) { value in
+                        gridSwitch()
+                    }
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10){
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    } // loop
+                } // grid
+                .animation(.easeIn)
+                .onAppear {
+                    gridSwitch()
+                }
+            } // vstack
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } // scroll
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
